f604cd8eaca6760a1ae6e9f75534991260796379
Daniel Noonan <noonan_dr@yahoo.co.uk>
2019-06-03 21:55:04 +0100

    feat(survey-ships): #SS-003 - create SurveyShips view

--- src/views/SurveyShips.vue
+++ src/views/SurveyShips.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    home
+  <div class="SurveyShips">
+    SurveyShips
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'home'
+  name: 'SurveyShips'
 }
 </script>
